5139bb7f8ed0d0081efc3a760a34adbbfe219b0c
JaneMansell <jm2992@bath.ac.uk>
2020-03-19 08:05:54 +0000

    Correction of spelling. (Responce has been changed to response.)

--- newbank/client/ExampleClient.java
+++ newbank/client/ExampleClient.java
@@ -12,25 +12,25 @@ public class ExampleClient extends Thread{
 	private Socket server;
 	private PrintWriter bankServerOut;	
 	private BufferedReader userInput;
-	private Thread bankServerResponceThread;
+	private Thread bankServerResponseThread;
 	
 	public ExampleClient(String ip, int port) throws UnknownHostException, IOException {
 		server = new Socket(ip,port);
 		userInput = new BufferedReader(new InputStreamReader(System.in)); 
 		bankServerOut = new PrintWriter(server.getOutputStream(), true); 
 		
-		bankServerResponceThread = new Thread() {
+		bankServerResponseThread = new Thread() {
 			private BufferedReader bankServerIn = new BufferedReader(new InputStreamReader(server.getInputStream())); 
 			public void run() {
 				while (true) {
 					try {
-						String responce = bankServerIn.readLine();
+						String response = bankServerIn.readLine();
 						// Kludge to exit on failed password
-						if ((responce == null)) {
+						if ((response == null)) {
 							server.close();
 							System.exit(0);
 						}
-						System.out.println(responce);
+						System.out.println(response);
 					} catch (IOException e) {
 						e.printStackTrace();
 						return;
@@ -38,7 +38,7 @@ public class ExampleClient extends Thread{
 				}
 			}
 		};
-		bankServerResponceThread.start();
+		bankServerResponseThread.start();
 	}
 	
 	
