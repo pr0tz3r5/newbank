d12e389836c0b0c10440b93cbc7a453e6779c844
jacky <khp32@bath.ac.uk>
2020-03-29 18:00:51 +0800

    Password check with length at least 8 in Customer.changePassword

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -24,6 +24,10 @@ public class Customer {
 	}
 
 	public Boolean changePassword(CustomerID customer, String password) {
+		if (password.length()<=8) {  // add to check the password length
+			System.out.println("Password length should be at least 8");
+			return false;
+		}
 		if (this.checkPasswdFormat(password)) {
 			passwd = password;
 			System.out.println("Password changed for " + customer.getKey());
