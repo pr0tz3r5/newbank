67549dfae0c172db641f3b291335f88b52c3ad29
JaneMansell <jm2992@bath.ac.uk>
2020-04-03 18:20:49 +0100

    Comments for CustomerID.java.

--- newbank/server/CustomerID.java
+++ newbank/server/CustomerID.java
@@ -1,3 +1,14 @@
+/************************************************************************
+ *             CustomerID.java   										*
+ * 																		*
+ * This is the class that for the object CustomerID.					*
+ * The constructor is of the form 										*
+ * CustomerID (String key).												*
+ * It contains a getter to return the key.								*
+ * It is used in NewBank.java to authorise the customer with the server	*
+ * when logging in.														*
+ * **********************************************************************/
+
 package newbank.server;
 
 public class CustomerID {
